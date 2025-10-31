affb99a35f50720f1416b61b818cd8435bc3d830
Gabriel <gmenden1@byu.edu>
2025-10-31 13:29:28 -0600

    Adding DatabaseManager to Github?

---
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -0,0 +1,124 @@
+package dataaccess;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class DatabaseManager {
+    private static String databaseName;
+    private static String dbUsername;
+    private static String dbPassword;
+    private static String connectionUrl;
+
+    private static final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS user (
+            username VARCHAR(256),
+            password LONGTEXT,
+            email VARCHAR(256)
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS game (
+            gameID INT,
+            whiteUsername VARCHAR(20),
+            blackUsername VARCHAR(20),
+            gameName VARCHAR(20),
+            chessGame LONGTEXT
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+            authToken LONGTEXT,
+            username LONGTEXT
+            );
+            """
+
+    };
+    /*
+     * Load the database information for the db.properties file.
+     */
+    static {
+        loadPropertiesFromResources();
+        try {
+            initializeTables();
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException("The runtime of this program was too long!");
+        }
+
+
+    }
+
+    /**
+     * Creates the database if it does not already exist.
+     */
+    static public void createDatabase() throws DataAccessException {
+        var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+        try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create database", ex);
+        }
+    }
+
+    /**
+     * Create a connection to the database and sets the catalog based upon the
+     * properties specified in db.properties. Connections to the database should
+     * be short-lived, and you must close the connection when you are done with it.
+     * The easiest way to do that is with a try-with-resource block.
+     * <br/>
+     * <code>
+     * try (var conn = DatabaseManager.getConnection()) {
+     * // execute SQL statements.
+     * }
+     * </code>
+     */
+    static Connection getConnection() throws DataAccessException {
+        try {
+            //do not wrap the following line with a try-with-resources
+            var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+            conn.setCatalog(databaseName);
+            return conn;
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get connection", ex);
+        }
+    }
+
+    private static void loadPropertiesFromResources() {
+        try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+            if (propStream == null) {
+                throw new Exception("Unable to load db.properties");
+            }
+            Properties props = new Properties();
+            props.load(propStream);
+            loadProperties(props);
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties", ex);
+        }
+    }
+
+    private static void loadProperties(Properties props) {
+        databaseName = props.getProperty("db.name");
+        dbUsername = props.getProperty("db.user");
+        dbPassword = props.getProperty("db.password");
+
+        var host = props.getProperty("db.host");
+        var port = Integer.parseInt(props.getProperty("db.port"));
+        connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
+    }
+
+
+    public static void initializeTables() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(String.format("Unable to configure database: %s", ex.getNextException()));
+        }
+    }
+}
